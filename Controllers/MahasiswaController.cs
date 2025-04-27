6f187c7c19cc6cd4f4b327b9d1a1d98423aa5a78
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-04-27 19:20:35 +0700

    menambahkan API mahasiswa

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        // Static list untuk menyimpan data mahasiswa
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Salman Al Farizy", "103022300113"),
+            new Mahasiswa("LeBron James", "1302000001"),
+            new Mahasiswa("Stephen Curry", "1302000002"),
+            new Mahasiswa("Kevin De Bruyne", "1302000003")
+
+        };
+
+
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return Ok(mahasiswaList);
+        }
+
+        // GET /api/mahasiswa/{index}
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            return Ok(mahasiswaList[index]);
+        }
+
+        // POST /api/mahasiswa
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mahasiswa)
+        {
+            mahasiswaList.Add(mahasiswa);
+            return CreatedAtAction(nameof(GetByIndex), new { index = mahasiswaList.Count - 1 }, mahasiswa);
+        }
+
+        // DELETE /api/mahasiswa/{index}
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            mahasiswaList.RemoveAt(index);
+            return NoContent();
+        }
+    }
+}
---
+++ Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            Nama = nama;
+            Nim = nim;
+        }
+    }
+}
